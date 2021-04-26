972112fecc5c6b10c985709b47b8c545cf6f7ba5
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-26 13:48:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 // Your code here
 
+class Cat{
+  
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  
+  speak(){
+    return `${this.name} says meow!`;
+  }
+  
+}
+
+class Dog{
+  
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  
+}
+
+class Bird{
+  
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  
+}
